fc5fd5216b90adba376e43a118640ed0720bd688
k2wl <k2wl@github.com>
2014-07-30 13:50:55 +0530

    mokee kk_mkt device tree for s4 mini duos i9192 serranodsdd

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsxx TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsxx/serranods:4.4.2/KOT49H/I9192XXUCNG2:user/release-keys" PRIVATE_BUILD_DESC="serranodsxx-user 4.4.2 KOT49H I9192XXUCNG2 release-keys"
 
 PRODUCT_DEVICE := serranodsdd
-PRODUCT_NAME := cm_serranodsdd
+PRODUCT_NAME := mk_serranodsdd
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_serranodsdd-userdebug
+add_lunch_combo mk_serranodsdd-userdebug
