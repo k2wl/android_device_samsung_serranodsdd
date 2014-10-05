edc9f6627a7bc89c335c0b3a53a69e7b9a8866e5
k2wl <k2wl@github.com>
2014-10-06 00:00:04 +0530

    add init.serranods.rc copy to ramdisk
    Change-Id: If589b33e5472f619cb67846e52ab17196c92602d

--- device.mk
+++ device.mk
@@ -23,7 +23,8 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     device/samsung/serranodsdd/init.carrier.rc:root/init.carrier.rc \
-
+    device/samsung/serranodsdd/init.carrier.rc:root/init.serranods.rc \
+    
 # Operator name
 PRODUCT_COPY_FILES += \
     device/samsung/serranodsdd/plmn_se13.bin:system/etc/plmn_se13.bin \
