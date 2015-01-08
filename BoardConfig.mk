01e64a84a7ecc30801b83359d3b4c05e0bf85d53
k2wl <k2wl@github.com>
2015-01-08 09:36:08 +0530

    Disable generic BLN
    Conflicts with ambient display
    Signed off by Arne Coucheron <arco68@gmail.com>
    
    Change-Id: I068639d64bdb794f361b7017f20d55ff9290d37a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,3 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8930_serrano_eur_3g_defconfig
 
 # Radio/Ril class for i9192
 BOARD_RIL_CLASS := ../../../device/samsung/serranodsdd/ril
-
-# BLN
-BOARD_HAVE_GENERIC_BLN := true
